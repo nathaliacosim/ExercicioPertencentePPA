2940aeddf29c735cdefc036489a2ad4018fda100
nathaliacosim <nathalia.cosim@novaandradina.org>
2023-06-05 10:19:31 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var ppa = Formulinha.AnoPertencentePpa(2000);
+        Console.WriteLine(ppa);
+    }
+}
+
+public class Formulinha
+{
+    public static int AnoPertencentePpa(int anoRef)
+    {
+        int base_ = 1998;
+        int x = (anoRef - base_) / 4;
+        int y = base_ + x * 4;
+
+        return y;
+    }
+}
